9591cb13f353d438152f098d7080c655c5da97b9
Florian Lenz <flenz81@googlemail.com>
2018-01-31 15:11:26 +0100

    updated node

--- Node.js
+++ Node.js
@@ -4,6 +4,7 @@ const spdy = require('libp2p-spdy');
 const secio = require('libp2p-secio');
 const MulticastDNS = require('libp2p-mdns');
 const DHT = require('libp2p-kad-dht');
+const Railing = require('libp2p-railing');
 
 class Node extends libp2p {
     constructor (peerInfo, peerBook, options) {
@@ -22,13 +23,20 @@ class Node extends libp2p {
                     secio
                 ]
             },
+            DHT: DHT,
             //Add railing
             discovery: [
-                new MulticastDNS(peerInfo)
-            ],
-            dht: DHT
+                new MulticastDNS(peerInfo, 2000)
+            ]
         };
 
-        super(modules, peerInfo, peerBook, options)
+        if(options.railing){
+            modules.discovery.push(new Railing('/ip4/172.18.0.4/tcp/34405/ipfs/QmesHkBq4nEo8JEXDhEjjQTvnzCLGcSqrUgS7j2xXAWnuQ', 2000));
+        }
+
+        super(modules, peerInfo, peerBook, options);
+
     }
 }
+
+module.exports = Node;
